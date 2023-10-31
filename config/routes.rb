4fd1cf1f81891d823cd3dbdebd91af6e5242c2b6
wbosw <146214919+wbosw@users.noreply.github.com>
2023-10-31 02:11:19 +0000

    omnicalc 2 start

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "addition", :action => "show_addition_form" })
 
   get("/ad", { :controller => "addition", :action => "show_addition_form" })
-  get("/wizard_add", { :controller => "addition", :action => "add_these" })
+  get("/wizard_addition", { :controller => "addition", :action => "add_these" })
 
   get("/subtract", { :controller => "subtract", :action => "show_sub_form" })
   get("", { :controller => "subtraction", :action => "subtract" })
