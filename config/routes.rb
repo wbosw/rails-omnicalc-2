8c743ccb3437ef95009c0f412702c59e21e7d417
wbosw <146214919+wbosw@users.noreply.github.com>
2023-10-31 14:59:04 +0000

    Subtraction complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
 
   get("/wizard_addition", { :controller => "addition", :action => "add_these" })
 
-  get("/subtract", { :controller => "subtract", :action => "show_sub_form" })
-  get("", { :controller => "subtraction", :action => "subtract" })
+  get("/subtract", { :controller => "subtraction", :action => "show_sub_form" })
+  get("/wizard_subtract", { :controller => "subtraction", :action => "subtract" })
 
   get("/multiply", { :controller => "multiplication", :action => "multiplication" })
   get("/wizard_multiply", { :controller => "multiplication", :action => "multiply" })
