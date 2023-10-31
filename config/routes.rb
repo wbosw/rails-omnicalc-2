32483219a3165b9e20a1c37c6ad06324e95af5b9
wbosw <146214919+wbosw@users.noreply.github.com>
2023-10-31 14:46:47 +0000

    addition complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "addition", :action => "show_addition_form" })
 
-  get("/ad", { :controller => "addition", :action => "show_addition_form" })
+  get("/add", { :controller => "addition", :action => "show_addition_form" })
+
   get("/wizard_addition", { :controller => "addition", :action => "add_these" })
 
   get("/subtract", { :controller => "subtract", :action => "show_sub_form" })
